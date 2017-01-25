edad0e38852e877e09d301416c14d825b96c18f3
Maxtsuka <poli8807dt@gmail.com>
2017-01-25 17:19:44 +0800

    Link

---
+++ iOS/LoginFlow/LoginVuewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace Maxtsuka.iOS
+{
+	public partial class LoginVuewController : UIViewController
+	{
+		public LoginVuewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			btnLogin.TouchUpInside += (sender, e) =>
+			{
+				PerformSegue("moveToMenu", this);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ iOS/MenuFlowController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+
+namespace Maxtsuka.iOS
+{
+	public partial class MenuFlowController : UIViewController
+	{
+		public MenuFlowController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			btnMap.TouchUpInside += (sender, e) =>
+			{
+				PerformSegue("moveToMap", this);
+			};
+
+			btnWeb.TouchUpInside += (sender, e) =>
+			{
+				PerformSegue("moveToWeb", this);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
